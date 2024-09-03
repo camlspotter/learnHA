c02bdf206790417643881f76d5bef04ad3b68d65
Jun Furuse <jun.furuse@dailambda.jp>
2024-09-03 22:38:47 +0900

    Adds test_range.py

--- infer_ha/range.py
+++ infer_ha/range.py
@@ -14,9 +14,3 @@ class Range:
 
     def pick_random_point(self, rng : random.Random) -> float:
         return rng.uniform(self.min, self.max)
-
-assert Range(1,2).contains(1.5), "Range.contains failure"
-ex_Range : Range = Range(1, 2)
-assert str(ex_Range) == "Range(min=1.0, max=2.0)", "Range bug:" + str(ex_Range)
-ex_Range2 : Range = Range(min=1, max=2)
-assert str(ex_Range2) == "Range(min=1.0, max=2.0)", "Range bug" + str(ex_Range)
