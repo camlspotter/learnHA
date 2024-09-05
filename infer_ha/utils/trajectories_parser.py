d0efbdb473d48363310336615ac9520b77f5f772
Jun Furuse <jun.furuse@dailambda.jp>
2024-09-06 08:46:58 +0900

    Simplifies trajectories_parser.py

--- infer_ha/utils/trajectories_parser.py
+++ infer_ha/utils/trajectories_parser.py
@@ -8,49 +8,6 @@ from infer_ha.trajectories import Trajectory, Trajectories
 def preprocess_trajectories(list_of_trajectories : list[Trajectory]) -> tuple[ list[NDArray[np.float64]],
                                                                                list[NDArray[np.float64]],
                                                                                list[tuple[int, int]] ]:
-    """
-    Converts list of trajectories into a single trajectory.
-    We do this conversion in order to avoid discarding 2M data-points from each trajectory during our segmentation
-    process (see function diff_method_backandfor() in module compute_derivatives.py).
-
-    :param
-        list_of_trajectories: Each element of the list is a trajectory. A trajectory is a 2-tuple of (time, vector),
-
-        where
-
-        time: is a list having a single item. The item is the sampling time, stored as a numpy.ndarray having structure as
-           (rows, ) where rows is the number of sample points. The dimension cols is empty meaning a single dim array.
-        vector: is a list having a single item. The item is a numpy.ndarray with (rows,cols), rows indicates the number of
-           points and cols as the system's dimension. The dimension is the total number of variables in the trajectories
-           including both input and output variables.
-
-    :return:
-        The lists t_list and y_list containing time and vector as (t_list, y_list) pair and positions.
-        Where
-        t_list: a single-item list whose item is a numpy.ndarray containing time-values as a concatenated list.
-        y_list: a single-item list whose item is a numpy.ndarray containing vector of values (of input and output) as a
-            concatenated list of trajectories.
-    """
-
-    t_list : list[NDArray[np.float64]] = []
-    y_list : list[NDArray[np.float64]] = []
-    position = []
-
-    if len(list_of_trajectories) == 1:  # when list_of_trajectories is a single trajectory
-        t_list, y_list = list_of_trajectories[0]
-        # print("len of t_list =", len(t_list))
-        # print("len of t_list[0] =", len(t_list[0]))
-        posi=(0, len(t_list[0]))
-        position.append(posi)
-    else:
-        t_list, y_list, position = convert_trajectories_to_single_list(list_of_trajectories)
-
-    return t_list, y_list, position
-
-
-def convert_trajectories_to_single_list(list_of_trajectories : list[Trajectory]) -> tuple[ list[NDArray[np.float64]],
-                                                                                           list[NDArray[np.float64]],
-                                                                                           list[tuple[int, int]] ]:
     '''
     This function performs the actual conversion of the list of trajectories into a single trajectory.
 
@@ -66,6 +23,7 @@ def convert_trajectories_to_single_list(list_of_trajectories : list[Trajectory])
         t_list: a single-item list whose item is a numpy.ndarray containing time-values as a concatenated list
         y_list: a single-item list whose item is a numpy.ndarray containing vector of values (of input and output) as a
                 concatenated list of trajectories.
+        ranges: the ranges of the Trajectories in the lists.
     '''
 
     t_list = []
@@ -104,22 +62,11 @@ def convert_trajectories_to_single_list(list_of_trajectories : list[Trajectory])
     t_list.append(temp_t_array_all) # converting the array back to list containing a single item
     y_list.append(temp_y_array_all)
 
-    # '''
-    # print("t_list is ", t_list)
-    # print("shape of t_list=", t_list[0].shape)
-    # print("type of t_list=", type(t_list[0]))
-    # print("y_list is ", y_list)
-    # print("shape of y_list=", y_list[0].shape)
-    # print("type of y_list=", type(y_list[0]))
-    # print("position = ", position)
-    # '''
-
     return t_list, y_list, position
 
 def parse_trajectories(input_filename : str) -> Trajectories:
     """
-    This is a special case function, because the argument input_filename is a filename which contains all
-    trajectories concatenated into a single file.
+    input_filename is a filename which contains all trajectories concatenated into a single file.
     This function parse this single file containing concatenated trajectories. Note that there is no symbols or special
     marker to separate trajectories. Each trajectory data are appended one below the other. The only indication is the
     value of the first column (contains the simpling time value). For a new trajectory this column value is always 0.0
@@ -134,11 +81,9 @@ def parse_trajectories(input_filename : str) -> Trajectories:
             vector: is a list having a single item. The item is a numpy.ndarray with (rows,cols), rows indicates the number of
                 points and cols as the system's dimension. The dimension is the total number of variables in the trajectories
                 including both input and output variables.
-        stepsize: is the sampling time period between two points.
-        system_dimension: is the dimension (input + output variables) of the system whose trajectory is being parsed.
-
+        stepsize: is the sampling time period between two points. The function assumes that the sampling time is constant
+            throughout all the tranjectories.
     """
-
 
     t_list : list[NDArray[np.float64]] = []
     y_list : list[NDArray[np.float64]] = []
@@ -193,29 +138,7 @@ def parse_trajectories(input_filename : str) -> Trajectories:
     trajectory = (t_list, y_list)  # create a tuple of (time and vector of list of single item, where item is np.array)
     list_of_trajectories.append(trajectory)  # create a list of trajectories
 
-#     '''
-#     print("y_list = ", y_list)
-#     print("totalPoints = ", totalPoints)
-#     print("totalPoints in y_list = ", len(y_list))
-# 
-#     print ("Type of t_list = ", type(t_list))
-#     print("Type of type(t_list[0]) = ", type(t_list[0]))
-#     print("Type of t_list[0].shape= ", t_list[0].shape)
-#     print("Type of t_list[0].shape[0]= ", t_list[0].shape[0])
-#     # print("Type of t_list[0].shape[1]= ", t_list[0].shape[1]) Error coz only one dimension available and no cols
-#     print ("t_list = ", t_list)
-# 
-#     print("Type of y_list = ", type(y_list))
-#     print("Type of type(y_list[0]) = ", type(y_list[0]))
-#     print("Type of y_list[0].shape= ", y_list[0].shape)   # prints the shape of y_list (15015, 5) = (rows, colmns)
-#     print("Type of y_list[0].shape[1]= ", y_list[0].shape[1])   # shape[0] for rows or records and shape[1] is cols or dimension
-#     # print("y_list = ", y_list)
-#     '''
-    # print("t_list = ", t_list)
-    # print ("t_list[0][2] = ", t_list[0][2])
-    # print ("t_list[0][1] = ", t_list[0][1])
     stepsize = t_list[0][2] - t_list[0][1]  # = 0.1 Computing the step-size from the sampled trajectories
-    # print("\nComputed Step-size = ", stepsize)
 
     return Trajectories( trajectories= list_of_trajectories,
                          stepsize= stepsize )
