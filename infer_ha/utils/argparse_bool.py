21c4ed45c52ae24c864eae07d509c1b5ea4110d3
Jun Furuse <jun.furuse@dailambda.jp>
2024-09-06 09:44:15 +0900

    Adds comments

--- infer_ha/utils/argparse_bool.py
+++ infer_ha/utils/argparse_bool.py
@@ -1,4 +1,6 @@
 # Fix of type=bool of argparse
+# type=bool is NOT supported by argparse, but it is NOT rejected and behaves strangely. See:
+#   https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
 def argparse_bool(x : str) -> bool:
     match x.lower():
         case "true":
