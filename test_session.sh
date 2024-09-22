f444b7ccbbac0013ef9a11686ea97a9ac43bc8d1
Jun Furuse <jun.furuse@dailambda.jp>
2024-09-22 15:21:15 +0900

    Big namespace refactoring.  No real code change.

--- test_session.sh
+++ test_session.sh
@@ -6,7 +6,7 @@ SIMULATION_OPTS="--time-horizon 13.0 --sampling-time 0.001 --fixed-interval-data
 eval "pipenv run python simulate.py $VARIABLE_OPTS $SIMULATION_OPTS --simulink-model-file ../../src/test_cases/engine/learn_ha_loop/ex_sldemo_bounce_Input.slx -o _out/bball.txt -n 8"
 
 # Annotations are currently indexed by xi variables
-eval "pipenv run python run.py $VARIABLE_OPTS -i _out/bball.txt --output-directory _out -c dtw -d 1 -m 1 -b 1 --segmentation-error-tol 0.1 --segmentation-fine-error-tol 0.9 --threshold-distance 9.0 --threshold-correlation 0.8 --dbscan-eps-dist 0.01 --dbscan-min-samples 2 --lmm-step-size 5 --is-invariant False --filter-last-segment True --annotations '{u:continuous,v:constant(0)}' "
+eval "pipenv run python inference.py $VARIABLE_OPTS -i _out/bball.txt --output-directory _out -c dtw -d 1 -m 1 -b 1 --segmentation-error-tol 0.1 --segmentation-fine-error-tol 0.9 --threshold-distance 9.0 --threshold-correlation 0.8 --dbscan-eps-dist 0.01 --dbscan-min-samples 2 --lmm-step-size 5 --is-invariant False --filter-last-segment True --annotations '{u:continuous,v:constant(0)}' "
 
 pipenv run python compile_ha.py --ode-solver-type fixed --ode-solver FixedStepAuto --invariant-mode 2 _out/learned_HA.json
 
