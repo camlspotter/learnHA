daedd20c9aa2815fd941d4fed9e2db83ab0afc06
Jun Furuse <jun.furuse@dailambda.jp>
2024-09-22 17:25:26 +0900

    Extends inference to take multiple trajectories files

--- hybridlearner/trajectory/trajectory.py
+++ hybridlearner/trajectory/trajectory.py
@@ -65,12 +65,12 @@ def parse_trajectories_files(paths : list[str]) -> Trajectories:
     trs_list = [ parse_trajectories(path) for path in paths ]
 
     stepsize = trs_list[0].stepsize
-    nvars = trs_list[0].trajectories[0][0].shape[1]
+    nvars = trs_list[0].trajectories[0][1].shape[1]
 
     if not all(lambda trs: trs.stepsize == stepsize for trs in trs_list):
         assert False, "Trajectories files must have a unique stepsize"
 
-    if not all(lambda trs: trs.trajectories[0][0].shape[1] == nvars for trs in trs_list):
+    if not all(lambda trs: trs.trajectories[0][1].shape[1] == nvars for trs in trs_list):
         assert False, "Trajectories files must have the same number of variables"
 
     trajectories = [ tr for trs in trs_list for tr in trs.trajectories ]
