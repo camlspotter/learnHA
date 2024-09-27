7287ae879c4984f09b9f1bbbb91d9fc2b14393ff
Jun Furuse <jun.furuse@dailambda.jp>
2024-09-27 18:34:03 +0900

    Adds vandel_loop.sh

---
+++ vandel_loop.sh
@@ -0,0 +1,19 @@
+SIMULATION_OPTS=""
+
+rm -rf _out
+
+eval "pipenv run python loop.py \
+     \
+     --input-variables '' --output-variables 'x,y' \
+     \
+     --time-horizon 20.0 --sampling-time 0.01 \
+     --fixed-interval-data False \
+     --invariant '0.5 <= x && x <= 1.5 && 0.5 <= y && y <= 1.5' \
+     --number-of-cps '' --signal-types '' \
+     --simulink-model-file vandel/van_del_pol_oscillator.slx -n 10 \
+     \
+     --output-directory vandel/ -c dtw -d 1 -m 1 -b 1 --segmentation-error-tol 0.1 --segmentation-fine-error-tol 0.9 --threshold-distance 9.0 --threshold-correlation 0.8 --dbscan-eps-dist 0.01 --dbscan-min-samples 2 --lmm-step-size 5 --is-invariant False --filter-last-segment True --annotations ''" \
+     \
+     --ode-solver-type fixed --ode-solver FixedStepAuto --invariant-mode 2 \
+     \
+     --counter-example-threshold 1.0
