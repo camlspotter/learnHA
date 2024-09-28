612df5bef2cbae23105881eaaf6962b72b88d919
Jun Furuse <jun.furuse@dailambda.jp>
2024-09-28 22:19:53 +0900

    test_loop.sh: widens the initial values and tweaks thresholds to have 1 cluster

--- test_loop.sh
+++ test_loop.sh
@@ -8,13 +8,13 @@ pipenv run python loop.py \
      --simulink-model-file data/models/ex_sldemo_bounce_Input.slx \
      --time-horizon 13.0 --sampling-time 0.01 \
      --fixed-interval-data False \
-     --invariant 'u:(-9.9, -9.5), x:(5, 10), v:(3, 16)' \
+     --invariant 'u:(-15.0, -5.0), x:(5, 10), v:(3, 16)' \
      --number-of-cps 'u:4' --signal-types 'u:linear' \
      \
      --output-directory _out \
      -c dtw -d 1 -m 1 -b 1 \
-     --segmentation-error-tol 0.1 \
-     --threshold-distance 1000.0 --threshold-correlation 0.8 \
+     --segmentation-error-tol 0.01 \
+     --threshold-distance 100.0 --threshold-correlation 0.01 \
      --lmm-step-size 5 --is-invariant False --filter-last-segment True \
      --annotations 'u:continuous, x:constant(0)' \
      \
