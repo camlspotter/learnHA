bdba14670eed0976905d5e36becd54b48e431226
Jun Furuse <jun.furuse@dailambda.jp>
2024-10-03 13:33:09 +0900

    Moves examples under tests/

--- tests/2inputs/test.sh
+++ tests/2inputs/test.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 set -e
 
-rm -rf test_2inputs/_out
+OUTDIR=tests/2inputs/_out
 
-pipenv run python compile_ha.py --ode-solver-type fixed --ode-solver FixedStepAuto --invariant-mode 2 test_2inputs/test.json --output-file test_2inputs/_out/test.slx
+rm -rf $OUTDIR
+
+pipenv run python compile_ha.py --ode-solver-type fixed --ode-solver FixedStepAuto --invariant-mode 2 tests/2inputs/test.json --output-file $OUTDIR/test.slx
 
 pipenv run python loop.py \
      \
      --input-variables 'y,x' --output-variables 'h,g' \
      \
-     --simulink-model-file test_2inputs/_out/test.slx \
+     --simulink-model-file $OUTDIR/test.slx \
      --time-horizon 10.0 --sampling-time 0.1 \
      --fixed-interval-data False \
      --invariant 'y:(-10, 10), x:(-10, 10), h:(-10, 10), g:(-10,10)' \
      --number-of-cps 'y:5,x:5' --signal-types 'y:linear,x:linear' \
      \
-     --output-directory test_2inputs/_out \
+     --output-directory $OUTDIR \
      -c dtw -d 1 -m 1 -b 1 \
      --segmentation-error-tol 0.01 \
      --threshold-distance 100.0 --threshold-correlation 0.01 \
@@ -24,7 +26,8 @@ pipenv run python loop.py \
      --ode-solver-type fixed --ode-solver FixedStepAuto --invariant-mode 2 \
      \
      -n 10 \
-     --counter-example-threshold 1.0
+     --counter-example-threshold 1.0 \
+     --max-nloops 2
 
 # original
 # --threshold-distance 9.0 --threshold-correlation 0.8
--- tests/vandel/vandel.sh
+++ tests/vandel/vandel.sh
@@ -2,22 +2,24 @@
 
 set -e
 
-rm -rf vandel/_out
-mkdir vandel/_out
+OUTDIR=tests/vandel/_out
 
-pipenv run python compile_ha.py --ode-solver-type fixed --ode-solver FixedStepAuto --invariant-mode 2 vandel/van_del_pol_oscillator.json --output-file vandel/_out/van_del_pol_oscillator.slx
+rm -rf $OUTDIR
+mkdir $OUTDIR
+
+pipenv run python compile_ha.py --ode-solver-type fixed --ode-solver FixedStepAuto --invariant-mode 2 tests/vandel/van_del_pol_oscillator.json --output-file $OUTDIR/van_del_pol_oscillator.slx
 
 pipenv run python loop.py \
      \
      --input-variables '' --output-variables 'x,y' \
      \
-     --simulink-model-file vandel/_out/van_del_pol_oscillator.slx \
+     --simulink-model-file $OUTDIR/van_del_pol_oscillator.slx \
      --time-horizon 10.0 --sampling-time 0.001 \
      --fixed-interval-data False \
      --invariant 'x:(-5,5), y:(-5,5)' \
      --number-of-cps '' --signal-types '' \
      \
-     --output-directory vandel/_out \
+     --output-directory $OUTDIR \
      -c dtw -d 3 -m 1 -b 1 \
      --segmentation-error-tol 0.1 --segmentation-fine-error-tol 0.9 \
      --threshold-distance 9.0 --threshold-correlation 0.8 \
