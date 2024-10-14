7c71459dcb58d5fd4f46e3d5fcf8371aec6c224d
Jun Furuse <jun.furuse@dailambda.jp>
2024-10-15 08:48:18 +0900

    Adds slx.info.get_IOports and removes get_Inports and get_Outports

--- hybridlearner/slx/info.py
+++ hybridlearner/slx/info.py
@@ -1,29 +1,40 @@
 import os
 import tempfile
 import textwrap
+from typeguard import typechecked
 from hybridlearner.matlab import engine
 
 
-def get_Outports(fn: str) -> list[str]:
+# Force runtime typechecking, since engine is not typed at all
+@typechecked
+def getvar_list_str(var: str) -> list[str]:
+    res = engine.getvar(var)
+    # Annoying!  A singleton list is returned as non-list
+    # XXX What happens for the empty list?
+    if not isinstance(res, list):
+        res = [res]
+    return res
+
+
+def get_IOports(fn: str) -> tuple[list[str], list[str]]:
     fn = os.path.abspath(fn)
     with tempfile.NamedTemporaryFile(
-        mode='w', suffix='.m', prefix='get_Outports_'
+        mode='w', suffix='.m', prefix='get_IOports_'
     ) as oc:
         oc.write(
             textwrap.dedent(
                 f"""\
-            global res;
-            res='hello';
-            modelPath = "{fn}";
-            mdl = load_system(modelPath);
-            modelName = get_param(mdl, 'Name');
-            res = getfullname(Simulink.findBlocksOfType(mdl, 'Outport'));
-            disp(res);
-            """
+                global inports;
+                global outports;
+                inports='dummy';
+                outports='dummy';
+                modelPath = "{fn}";
+                mdl = load_system(modelPath);
+                inports = getfullname(Simulink.findBlocksOfType(mdl, 'Inport'));
+                outports = getfullname(Simulink.findBlocksOfType(mdl, 'Outport'));
+                """
             )
         )
         oc.flush()
         engine.run(oc.name)
-        s = engine.getvar('res')
-        print('result:', s)
-        return s
+        return (getvar_list_str('inports'), getvar_list_str('outports'))
--- hybridlearner/slx/test_info.py
+++ hybridlearner/slx/test_info.py
@@ -1,3 +1,10 @@
 from hybridlearner.slx.info import *
 
-get_Outports('data/models/ex_sldemo_bounce_Input.slx')
+assert get_IOports('data/models/ex_sldemo_bounce_Input.slx') == (
+    ['ex_sldemo_bounce_Input/uIn'],
+    [
+        'ex_sldemo_bounce_Input/Outport',
+        'ex_sldemo_bounce_Input/Outport1',
+        'ex_sldemo_bounce_Input/Outport2',
+    ],
+)
