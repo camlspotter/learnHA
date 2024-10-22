b35ae6d153900916ea78db68c75ca76981a1f10b
Jun Furuse <jun.furuse@dailambda.jp>
2024-10-22 10:17:12 +0900

    matlab: Removes debug print

--- hybridlearner/matlab.py
+++ hybridlearner/matlab.py
@@ -26,12 +26,12 @@ class Matlab:
 
     def setvar(self, var: str, val: Any) -> None:
         _eng = self.engine()
-        print(f"Set MATLAB variable {var}")
+        # print(f"Set MATLAB variable {var}")
         _eng.workspace[var] = val
 
     def getvar(self, var: str) -> Any:
         _eng = self.engine()
-        print(f"Get MATLAB variable {var}")
+        # print(f"Get MATLAB variable {var}")
         return _eng.workspace[var]
 
 
