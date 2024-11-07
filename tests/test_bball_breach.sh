0b9d0eafe22a99d3cdc0c810621638b01c01171f
Jun Furuse <jun.furuse@dailambda.jp>
2024-11-07 23:12:39 +0900

    test_bball_breach.sh: fixes parameters

--- tests/test_bball_breach.sh
+++ tests/test_bball_breach.sh
@@ -6,9 +6,9 @@ pipenv run python loop_breach.py \
      --input-variables 'u' --output-variables 'x,v' \
      \
      --simulink-model-file data/models/ex_sldemo_bounce_Input.slx \
-     --time-horizon 13.0 --sampling-time 0.01 \
+     --time-horizon 10.0 --sampling-time 0.01 \
      --fixed-interval-data False \
-     --invariant 'u:(-15.0, -5.0), x:(5, 10), v:(3, 16)' \
+     --invariant 'u:(-15.0, -5.0), x:(5, 10), v:(-10, 16)' \
      --number-of-cps 'u:4' --signal-types 'u:linear' \
      \
      --output-directory _out/bball_breach \
@@ -21,5 +21,5 @@ pipenv run python loop_breach.py \
      --ode-solver-type fixed --ode-solver FixedStepAuto --invariant-mode 2 \
      \
      -n 20 \
-     --counter-example-threshold 1.0 \
+     --counter-example-threshold 2.0 \
      --max-nloops 10
