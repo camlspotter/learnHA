d3b98e16cca0d1f21d1cfa43825cd21d7079778b
Jun Furuse <jun.furuse@dailambda.jp>
2024-11-09 18:35:44 +0900

    test_bball_breach: makes it shorter (in time)

--- tests/test_bball_breach.sh
+++ tests/test_bball_breach.sh
@@ -20,6 +20,6 @@ pipenv run python loop_breach.py \
      \
      --ode-solver-type fixed --ode-solver FixedStepAuto --invariant-mode 2 \
      \
-     -n 20 \
-     --counter-example-threshold 2.0 \
-     --max-nloops 10
+     -n 10 \
+     --counter-example-threshold 15.0 \
+     --max-nloops 5
