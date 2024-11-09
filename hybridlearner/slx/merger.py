f13e5bab3ce04c8187d070361af46aa27329de73
Jun Furuse <jun.furuse@dailambda.jp>
2024-11-09 18:34:58 +0900

    slx.merger: removes the output ports of input variables

--- hybridlearner/slx/merger.py
+++ hybridlearner/slx/merger.py
@@ -1,12 +1,55 @@
-# Merge 2 SLX models into 1
-# The models must share the same IO ports.
-
 import os
 from io import TextIOWrapper
 import textwrap
 
 
-def merge_without_save(out: TextIOWrapper, fn_a: str, fn_b: str, fn_merged: str) -> str:
+def merge(
+    out: TextIOWrapper,
+    fn_a: str,
+    fn_b: str,
+    fn_merged: str,
+    input_variables: list[str],
+    output_variables: list[str],
+) -> str:
+    '''
+    Merge 2 SLX models into 1
+
+    The models must share the same IO ports:
+
+      - Their input ports are for the input variables.
+      - Their output ports are for the output and input variables:
+        the outputs come first then the inputs follow.
+
+    The merged model has the same number of input ports of the names of input variables.
+    The merged model has 2 sets of output ports:
+
+      - a_v: for the output variable v of the first model
+      - b_v: for the output variable v of the second model
+
+    The merged model has another set of output ports diff_v for the differences
+    of the output ports a_v and b_v of the models.  The difference is computed as
+    the integral of the absolute difference of the 2 signals.
+
+    Try
+      pipenv run python3 -m unittest hybridlearner.slx.test_merger
+    then check
+      _out/merged.slx
+    '''
+    merged = merge_without_save(
+        out, fn_a, fn_b, fn_merged, input_variables, output_variables
+    )
+    save_system(out, fn_merged, merged)
+    return merged
+
+
+def merge_without_save(
+    out: TextIOWrapper,
+    fn_a: str,
+    fn_b: str,
+    fn_merged: str,
+    input_variables: list[str],
+    output_variables: list[str],
+) -> str:
     a = os.path.splitext(os.path.basename(fn_a))[0]
     b = os.path.splitext(os.path.basename(fn_b))[0]
     merged = os.path.splitext(fn_merged)[0]
@@ -14,14 +57,9 @@ def merge_without_save(out: TextIOWrapper, fn_a: str, fn_b: str, fn_merged: str)
     fn_a = os.path.abspath(fn_a)
     fn_b = os.path.abspath(fn_b)
 
-    merge_system(out, fn_a, fn_b, fn_merged, a, b, merged)
-    connect_ports(out, fn_a, fn_b, fn_merged, a, b, merged)
-    return merged
-
-
-def merge(out: TextIOWrapper, fn_a: str, fn_b: str, fn_merged: str) -> str:
-    merged = merge_without_save(out, fn_a, fn_b, fn_merged)
-    save_system(out, fn_merged, merged)
+    merge_system(
+        out, fn_a, fn_b, fn_merged, a, b, merged, input_variables, output_variables
+    )
     return merged
 
 
@@ -33,53 +71,38 @@ def merge_system(
     a: str,
     b: str,
     merged: str,
+    input_variables: list[str],
+    output_variables: list[str],
 ) -> None:
     out.write(
         textwrap.dedent(
             f"""\
-        % bdclose all;
-        % clear;
-
-        % Load the original model
-        load_system('{fn_a}');
-        load_system('{fn_b}');
-        
-        % New empty model, which will be saved as {fn_merged}
-        h = new_system();
-        set_param(h, 'Name', '{merged}')
-
-        % Make a subsystem in the new model at {merged}/{a}
-        add_block('built-in/Subsystem', '{merged}/{a}');
-
-        % Copy the contents of the original to {merged}/{a}:
-        Simulink.BlockDiagram.copyContentsToSubsystem('{a}', '{merged}/{a}');
- 
-        % Make a subsystem in the new model at {merged}/{b}
-        add_block('built-in/Subsystem', '{merged}/{b}');
- 
-        % Copy the contents of the learned to {merged}/{b}:
-        Simulink.BlockDiagram.copyContentsToSubsystem('{b}', '{merged}/{b}');
- 
-        % Arrange the subsystem positions automatically
-        Simulink.BlockDiagram.arrangeSystem('{merged}');
-
-        """
-        )
-    )
-
-
-def connect_ports(
-    out: TextIOWrapper,
-    fn_a: str,
-    fn_b: str,
-    fn_merged: str,
-    a: str,
-    b: str,
-    merged: str,
-) -> None:
-    out.write(
-        textwrap.dedent(
-            f"""
+            bdclose all;
+            % clear;
+    
+            % Load the original model
+            load_system('{fn_a}');
+            load_system('{fn_b}');
+            
+            % New empty model, which will be saved as {fn_merged}
+            h = new_system();
+            set_param(h, 'Name', '{merged}')
+    
+            % Make a subsystem in the new model at {merged}/{a}
+            add_block('built-in/Subsystem', '{merged}/{a}');
+    
+            % Copy the contents of the original to {merged}/{a}:
+            Simulink.BlockDiagram.copyContentsToSubsystem('{a}', '{merged}/{a}');
+     
+            % Make a subsystem in the new model at {merged}/{b}
+            add_block('built-in/Subsystem', '{merged}/{b}');
+     
+            % Copy the contents of the learned to {merged}/{b}:
+            Simulink.BlockDiagram.copyContentsToSubsystem('{b}', '{merged}/{b}');
+     
+            % Arrange the subsystem positions automatically
+            Simulink.BlockDiagram.arrangeSystem('{merged}');
+    
             %% Connect ports
  
             aPorts = get_param('{merged}/{a}', 'PortHandles');
@@ -90,87 +113,110 @@ def connect_ports(
 
             %% In-ports
 
-            % in_i ---+---> Inport(i) of a
-            %         |
-            %         +---> Inport(i) of b 
-
-            for i = 1:length(aPorts.Inport)
-                inport = sprintf('{merged}/in_%d', i);
-
-                add_block('simulink/Sources/In1', inport);
-    
-                xiInput = get_param(inport, 'PortHandles');
-                % port number is automatically assigned.
-                % set_param(inport, 'Port', num2str(i));
-                set_param(inport, 'SignalType', 'auto');
-                add_line('{merged}', xiInput.Outport(1), aPorts.Inport(i));
-                add_line('{merged}', xiInput.Outport(1), bPorts.Inport(i));
-            end
-
-            %% Out-ports
-
-            % Outport(i) of a ------> out_ai
-
-            for i = 1:length(aPorts.Outport)
-                outport = sprintf('{merged}/out_a%d', i);
-
-                add_block('simulink/Sinks/Out1', outport);
-                aOutPorts = get_param(outport, 'PortHandles');
-                % set_param(outport, 'Port', num2str(i));
-                set_param(outport, 'SignalType', 'auto');
-                add_line('{merged}', aPorts.Outport(i), aOutPorts.Inport(1));
-            end
-
-            % Outport(i) of b ------> out_bi  
-
-            for i = 1:length(bPorts.Outport)
-                outport = sprintf('{merged}/out_b%d', i);
-
-                add_block('simulink/Sinks/Out1', outport);
-                bOutPorts = get_param(outport, 'PortHandles');
-                % set_param(outport, 'Port', num2str(i + length(aPorts.Outport)));
-                set_param(outport, 'SignalType', 'auto');
-                add_line('{merged}', bPorts.Outport(i), bOutPorts.Inport(1));
-            end
-
-            % Outport(i) of a -->++--------+    +---+    +---------+     +-----+
-            %                     |Subtract| -> |Abs| -> |Integrate| --> |diffi|  
-            % Outport(i) of b -->-+--------+    +---+    +---------+     +-----+
-
-            % XXX No need to get diffs between input ports
-            for i = 1:length(aPorts.Outport)
-               diff = sprintf('{merged}/diff%d', i);
-               add_block('simulink/Sinks/Out1', diff);
-               diffports = get_param(diff, 'PortHandles');
-           
-               integrator = sprintf('{merged}/integrator%d', i);
-               add_block('simulink/Continuous/Integrator', integrator);
-               integratorports = get_param(integrator, 'PortHandles');
-           
-               add_line('{merged}', integratorports.Outport(1), diffports.Inport(1));
-           
-               abs = sprintf('{merged}/abs%d', i);
-               add_block('simulink/Math Operations/Abs', abs);
-               % Simulation does not terminate if ZeroCross = 'on' (default)
-               set_param(abs, 'ZeroCross', 'off');
-               absports = get_param(abs, 'PortHandles');
-           
-               add_line('{merged}', absports.Outport(1), integratorports.Inport(1));
-           
-               sub = sprintf('{merged}/sub%d', i);
-               add_block('simulink/Math Operations/Subtract', sub);
-               subports = get_param(sub, 'PortHandles');
-           
-               add_line('{merged}', aPorts.Outport(1), subports.Inport(1));
-               add_line('{merged}', bPorts.Outport(1), subports.Inport(2));
-               add_line('{merged}', subports.Outport(1), absports.Inport(1));
-            end
-
-            Simulink.BlockDiagram.arrangeSystem('{merged}');
-
+            % vi ---+---> Inport(i) of a
+            %       |
+            %       +---> Inport(i) of b 
             """
         )
     )
+
+    for i, iv in enumerate(input_variables):
+        out.write(
+            textwrap.dedent(
+                f"""\
+                inblock = '{merged}/{iv}';
+                add_block('simulink/Sources/In1', inblock);
+                inPorts = get_param(inblock, 'PortHandles');
+                set_param(inblock, 'SignalType', 'auto');
+                add_line('{merged}', inPorts.Outport(1), aPorts.Inport({i+1}));
+                add_line('{merged}', inPorts.Outport(1), bPorts.Inport({i+1}));
+
+                """
+            )
+        )
+
+    out.write(
+        textwrap.dedent(
+            f"""\
+            %% Out-ports
+
+            % Outport(i) of a ------> a_vi
+            """
+        )
+    )
+
+    for i, ov in enumerate(output_variables):
+        out.write(
+            textwrap.dedent(
+                f"""\
+                outblock = '{merged}/a_{ov}';
+                add_block('simulink/Sinks/Out1', outblock);
+                aOutPorts = get_param(outblock, 'PortHandles');
+                set_param(outblock, 'SignalType', 'auto');
+                add_line('{merged}', aPorts.Outport({i+1}), aOutPorts.Inport(1));
+                """
+            )
+        )
+
+    out.write("% Outport(i) of b ------> b_vi\n")
+
+    for i, ov in enumerate(output_variables):
+        out.write(
+            textwrap.dedent(
+                f"""\
+                outblock = '{merged}/b_{ov}';
+                add_block('simulink/Sinks/Out1', outblock);
+                bOutPorts = get_param(outblock, 'PortHandles');
+                set_param(outblock, 'SignalType', 'auto');
+                add_line('{merged}', bPorts.Outport({i+1}), bOutPorts.Inport(1));
+                """
+            )
+        )
+
+    out.write(
+        textwrap.dedent(
+            f"""\
+            % Outport(i) of a -->(+)+--------+    +---+    +---------+     +-------+
+            %                       |Subtract| -> |Abs| -> |Integrate| --> |diff_vi|  
+            % Outport(i) of b -->(-)+--------+    +---+    +---------+     +-------+
+            """
+        )
+    )
+
+    for i, ov in enumerate(output_variables):
+        out.write(
+            textwrap.dedent(
+                f"""\
+                diff = '{merged}/diff_{ov}';
+                add_block('simulink/Sinks/Out1', diff);
+                diffports = get_param(diff, 'PortHandles');
+           
+                integrator = '{merged}/integrator_{ov}';
+                add_block('simulink/Continuous/Integrator', integrator);
+                integratorports = get_param(integrator, 'PortHandles');
+           
+                add_line('{merged}', integratorports.Outport(1), diffports.Inport(1));
+           
+                abs = '{merged}/abs_{ov}';
+                add_block('simulink/Math Operations/Abs', abs);
+                % Simulation does not terminate if ZeroCross = 'on' (default)
+                set_param(abs, 'ZeroCross', 'off');
+                absports = get_param(abs, 'PortHandles');
+           
+                add_line('{merged}', absports.Outport(1), integratorports.Inport(1));
+           
+                sub = '{merged}/sub_{ov}';
+                add_block('simulink/Math Operations/Subtract', sub);
+                subports = get_param(sub, 'PortHandles');
+           
+                add_line('{merged}', aPorts.Outport({i+1}), subports.Inport(1));
+                add_line('{merged}', bPorts.Outport({i+1}), subports.Inport(2));
+                add_line('{merged}', subports.Outport(1), absports.Inport(1));
+                """
+            )
+        )
+
+    out.write(f"Simulink.BlockDiagram.arrangeSystem('{merged}');\n")
 
 
 def save_system(out: TextIOWrapper, fn_merged: str, merged: str) -> None:
--- hybridlearner/slx/test_merger.py
+++ hybridlearner/slx/test_merger.py
@@ -6,20 +6,19 @@ from hybridlearner.matlab import engine
 script = '_out/merge_models.m'
 
 with io.open_for_write(script) as oc:
-    # It builds _out/merged.slx
-    try:
-        os.remove('_out/merged.slx')
-    except OSError:
-        pass
-
     merge(
         oc,
         'data/models/ex_sldemo_bounce_Input.slx',
         'data/models/bball_learned_HA0.slx',
         'merged.slx',
+        ['u'],
+        ['x', 'v'],
     )
 
-engine.eval('bdclose all;', nargout=0)
-engine.eval('clear;', nargout=0)
+# It builds _out/merged.slx
+try:
+    os.remove('_out/merged.slx')
+except OSError:
+    pass
 
 engine.run(script)
