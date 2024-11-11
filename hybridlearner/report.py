be0c1f6a6e1da5ea8c5f23f02eaae6bec63b6b27
Jun Furuse <jun.furuse@dailambda.jp>
2024-11-11 19:51:08 +0900

    report: Adds report_all

--- hybridlearner/report.py
+++ hybridlearner/report.py
@@ -67,8 +67,7 @@ def report(
         counter_examples_top_10 = counter_examples[:10]
 
         header = (
-            ['time']
-            + input_variables
+            input_variables
             + [f"original:{k}" for k in output_variables]
             + [f"learned:{k}" for k in output_variables]
         )
@@ -88,12 +87,7 @@ def report(
             base = f"counter_examples{iteration:02d}.svg"
             counter_example_file = os.path.join(output_directory, base)
 
-            plot_timeseries_multi(
-                counter_example_file,
-                'Counter examples',
-                header[1:],  # drop 'time'
-                trs,
-            )
+            plot_timeseries_multi(counter_example_file, 'Counter examples', header, trs)
 
             log.write(
                 f"## Counter examples (Top {len(counter_examples_top_10)} of {len(counter_examples)})\n\n"
@@ -103,8 +97,7 @@ def report(
             log.write(f"## Counter examples\n\n")
             log.write(f"None!\n\n")
 
-    if shutil.which('pandoc'):
-        subprocess.run(f'pandoc {log_filename} -o {log_filename}.html', shell=True)
+    pandoc_html(log_filename)
 
 
 def report_no_comparison(
@@ -141,7 +134,7 @@ def report_no_comparison(
 
         counter_examples_top_10 = counter_examples[:10]
 
-        header = ['time'] + input_variables + [f"learned:{k}" for k in output_variables]
+        header = input_variables + [f"learned:{k}" for k in output_variables]
 
         trs = [
             (
@@ -161,7 +154,7 @@ def report_no_comparison(
             plot_timeseries_multi(
                 counter_example_file,
                 'Counter examples',
-                header[1:],  # drop 'time'
+                header,  # drop 'time'
                 trs,
             )
 
@@ -173,5 +166,89 @@ def report_no_comparison(
             log.write(f"## Counter examples\n\n")
             log.write(f"None!\n\n")
 
+    pandoc_html(log_filename)
+
+
+def report_all(
+    output_directory: str,
+    model_name: str,
+    iteration: int,
+    ha: HybridAutomaton,
+    counter_examples: list[tuple[Trajectory, float]],
+    passed: list[Trajectory],
+    input_variables: list[str],
+    output_variables: list[str],
+) -> None:
+    """
+    Counter example plot. Show both the original and learned for visual comparison.
+
+    - output_directory: where to produce the report and images
+    - model_name: used in the title
+    - iteration: used in the title
+    - ha : hybrid automaton
+    - counter_examples: counter examples found for ha
+    - input_variables: the names of the input variables
+    - output_variables: the names of the output variables
+    """
+    log_filename = os.path.join(output_directory, f'report{iteration:02d}.md')
+    with utils_io.open_for_write(log_filename) as log:
+        log.write(f"# {model_name} inference {iteration}\n\n")
+        log.write(f"## Automaton\n\n")
+        log.write("```\n")
+        ha.hum_print(log)
+        log.write("```\n")
+        log.write("\n")
+
+        # inplace sort by dist
+        counter_examples.sort(key=lambda x: -x[1])
+        counter_examples_top_10 = counter_examples[:10]
+
+        header = input_variables + [f"learned:{k}" for k in output_variables]
+
+        trs = [
+            (
+                f"Trajectory {i} (dist: {dist:.2f})",  # title
+                (
+                    lt[0],  # time
+                    lt[1],  # values
+                ),
+            )
+            for (i, (lt, dist)) in enumerate(counter_examples_top_10)
+        ]
+
+        if len(trs) != 0:
+            base = f"counter_examples{iteration:02d}.svg"
+            counter_example_file = os.path.join(output_directory, base)
+
+            plot_timeseries_multi(counter_example_file, 'Counter examples', header, trs)
+
+            log.write(
+                f"## Counter examples (Top {len(counter_examples_top_10)} of {len(counter_examples)})\n\n"
+            )
+            log.write(f"![]({base})\n\n")
+        else:
+            log.write(f"## Counter examples\n\n")
+            log.write(f"None!\n\n")
+
+        # Passed
+
+        passed_10 = passed[:10]
+        trs = [
+            (
+                f"Trajectory {i}",  # title
+                lt,
+            )
+            for (i, lt) in enumerate(passed_10)
+        ]
+        base = f"passedexamples{iteration:02d}.svg"
+        passed_example_file = os.path.join(output_directory, base)
+        plot_timeseries_multi(passed_example_file, 'Passed examples', header, trs)
+        log.write(f"## Passed examples ({len(passed_10)} of {len(passed)})\n\n")
+        log.write(f"![]({base})\n\n")
+
+    pandoc_html(log_filename)
+
+
+def pandoc_html(log_filename: str) -> None:
     if shutil.which('pandoc'):
         subprocess.run(f'pandoc {log_filename} -o {log_filename}.html', shell=True)
