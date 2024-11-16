a8f676c14fc63ce56c8474ed0644b8d9833e4608
Jun Furuse <jun.furuse@dailambda.jp>
2024-11-16 14:52:28 +0900

    falsify.breach: refactors matlab communication

--- hybridlearner/falsify/breach.py
+++ hybridlearner/falsify/breach.py
@@ -4,6 +4,7 @@ import random
 import numpy as np
 import textwrap
 from typing import Protocol
+from pydantic import ConfigDict
 from pydantic.dataclasses import dataclass
 from hybridlearner.types import Invariant
 from hybridlearner.utils import io as utils_io
@@ -49,6 +50,24 @@ class find_counter_examples_options:
     nsimulations: int
     counter_example_threshold: float
     skip_already_tried_parameters: bool
+
+
+@dataclass(config=ConfigDict(arbitrary_types_allowed=True))
+class Falsification_result:
+    counter_examples: list[
+        tuple[
+            Trajectory,  # original
+            Trajectory,  # learned
+            float,  # distance
+        ]
+    ]
+
+    passed_examples: list[
+        tuple[
+            Trajectory,  # original
+            Trajectory,  # learned
+        ]
+    ]
 
 
 class Falsifier:
@@ -105,31 +124,56 @@ def find_counter_examples_aux(
 
     engine.run(script_fn)
 
-    time = np.array(engine.getvar('time'))
-    if time.size == 0:
-        time = matlab.double([])
-        original_signals = matlab.double([])
-        learned_signals = matlab.double([])
-        scores = np.array([])
+    time = np.array(engine.getvar('time'))[0]
+
+    # Usually, if multiple ncounter examples found,
+    #   np.shape(signals_matlab) = (n, 3, 1001)
+    # (1001 is the number of the frames)
+    #
+    # However, if only 1 counter example found, np.shape(signals_matlab) = (3, 1001)
+    def fix_signals(signals: MATRIX) -> MATRIX:
+        match np.shape(signals):
+            case (_, _, _):
+                return signals
+            case (_, _):
+                print('Fixing the dimension of singals')
+                return signals[np.newaxis, :]
+            case _:
+                assert False
+
+    # F**king handling of corner cases of a Python library 😡😡😡!!
+    original_signals = fix_signals(np.array(engine.getvar('original_signals')))
+    learned_signals = fix_signals(np.array(engine.getvar('learned_signals')))
+
+    # scores can be empty! when only 1 counter example is found
+    scores = np.array(engine.eval('pb.obj_false', 1))
+    if scores.size == 0:
+        print('Strange scores:', scores, 'Fixing its dimension')
+        scores = np.array([0])
     else:
-        time = np.array(engine.getvar('time'))[0]
-        original_signals = engine.getvar('original_signals')
-        learned_signals = engine.getvar('learned_signals')
-        scores = np.array(engine.getvar('scores'))[0]
+        scores = scores[0]
+
+    print(
+        'time:',
+        np.shape(time),
+        'original_signals:',
+        np.shape(original_signals),
+        'learned_signals:',
+        np.shape(learned_signals),
+        'scores:',
+        np.shape(scores),
+    )
+    assert np.shape(original_signals)[0] == np.shape(scores)[0]
 
     original_trs = list(
         map(lambda sig: (time, np.transpose(np.array(sig))), original_signals)
     )
-
     learned_trs = list(
         map(lambda sig: (time, np.transpose(np.array(sig))), learned_signals)
     )
 
     tried_parameters = engine.getvar('tried_parameters')
     tried_obj_log = engine.getvar('tried_obj_log')
-
-    tried_parameters2: MATRIX = np.transpose(np.array(tried_parameters))
-    print('tried_parameters', tried_parameters2)
 
     return (
         [
@@ -154,6 +198,11 @@ def build_script(
     }
 
     with utils_io.open_for_write(script_fn) as out:
+        w = out.write
+
+        def wd(s: str) -> None:
+            w(textwrap.dedent(s))
+
         merged = merge(
             out,
             original_model_file,
@@ -163,56 +212,57 @@ def build_script(
             opts.output_variables,
         )
 
-        out.write("% MATLABPATH must contain Breach\n")
-        out.write("InitBreach;\n\n")
+        wd(f"""\
+        % MATLABPATH must contain Breach
+        InitBreach;
+
+        % Free variables must be assigned with dummy values for BreachSimulinkSystem
+        """)
 
         # Fill variables to load the model
-        out.write(
-            "% Free variables must be assigned with dummy values for BreachSimulinkSystem\n"
-        )
         for ov in opts.output_variables:
             idx = variable_index[ov]
-            out.write(f"a{idx} = 42; % for output variable {ov}\n")
+            w(f"a{idx} = 42; % for output variable {ov}\n")
 
-        out.write(f"\nBsim = BreachSimulinkSystem('{merged}');\n\n")
+        w(f"\nBsim = BreachSimulinkSystem('{merged}');\n\n")
+
+        parameters = []
 
         # Range of the initial output variables
         for ov in opts.output_variables:
             idx = variable_index[ov]
             r: Range = opts.invariant[ov]
-            out.write(f"% Range of the initial value of output variable {ov}\n")
-            out.write(f"Bsim.SetParamRanges({{'a{idx}'}}, [{r.min} {r.max}]);\n\n")
+            wd(f"""\
+            % Range of the initial value of output variable {ov}
+            Bsim.SetParamRanges({{'a{idx}'}}, [{r.min} {r.max}]);
+
+            """)
+            parameters.append(f'a{idx}')
 
         # Generators of the input variables
         for iv in opts.input_variables:
             ncps = opts.number_of_cps[iv]
             signal_type = opts.signal_types[iv]
             r = opts.invariant[iv]
-            out.write(f"% Input signal {iv} for input variable {iv}\n")
+            w(f"% Input signal {iv} for input variable {iv}\n")
             match signal_type:
                 case SignalType.FIXED_STEP:
-                    out.write(f"Bsim.SetInputGen('UniStep{ncps}');\n")
+                    w(f"Bsim.SetInputGen('UniStep{ncps}');\n")
                     for i in range(0, ncps):
-                        out.write(
-                            f"Bsim.SetParamRanges({{'{iv}_u{i}'}}, [{r.min} {r.max}]);\n"
-                        )
-                    out.write("\n")
+                        w(f"Bsim.SetParamRanges({{'{iv}_u{i}'}}, [{r.min} {r.max}]);\n")
+                        parameters.append(f'{iv}_u{i}')
+                    w("\n")
 
                 case SignalType.LINEAR:
-                    out.write(
-                        textwrap.dedent(
-                            f"""\
-                            input_gen.type = 'UniStep';
-                            input_gen.cp = {ncps};
-                            input_gen.method = {{'linear'}};
-                            Bsim.SetInputGen(input_gen);
-                            """
-                        )
-                    )
+                    wd(f"""\
+                    input_gen.type = 'UniStep';
+                    input_gen.cp = {ncps};
+                    input_gen.method = {{'linear'}};
+                    Bsim.SetInputGen(input_gen);
+                    """)
                     for i in range(0, ncps):
-                        out.write(
-                            f"Bsim.SetParamRanges({{'{iv}_u{i}'}}, [{r.min} {r.max}]);\n"
-                        )
+                        w(f"Bsim.SetParamRanges({{'{iv}_u{i}'}}, [{r.min} {r.max}]);\n")
+                        parameters.append(f'{iv}_u{i}')
 
         original_signal_names = (
             "{"
@@ -256,67 +306,69 @@ def build_script(
         ]
         stl_formula = f"alw ({' and '.join(stl_components)})"
 
-        out.write(
-            textwrap.dedent(
-                f"""\
-                Bsim.SetParam('timeStepMax', {opts.sampling_time}); % Probably the next line is enough.
-                Bsim.Sys.tspan = 0:{opts.sampling_time}:{opts.time_horizon};  % See the head comment in Core/Falsify.m
+        wd(f"""\
+        Bsim.SetParam('timeStepMax', {opts.sampling_time}); % Probably the next line is enough.
+        Bsim.Sys.tspan = 0:{opts.sampling_time}:{opts.time_horizon};  % See the head comment in Core/Falsify.m
+        
+        % Falsification
+        phi = STL_Formula('phi', '{stl_formula}');
+        R = BreachRequirement(phi);
+        pb = FalsificationProblem(Bsim,R);
+        pb.StopAtFalse=0 % more than 1 counter examples if found
+        pb.max_obj_eval = {max_obj_eval};
 
-                % Falsification
-                phi = STL_Formula('phi', '{stl_formula}');
-                R = BreachRequirement(phi);
-                pb = FalsificationProblem(Bsim,R);
-                pb.X_log = tried_parameters;
-                pb.obj_log = tried_obj_log;
-                pb.StopAtFalse=0 % more than 1 counter examples if found
-                pb.max_obj_eval = {max_obj_eval};
-                pb.solve();
-                falses = pb.GetFalse();
+        % Falsification cache
+        if ~exist('tried_parameters', 'var')
+            disp('tried_parameters is not defined. Use the default []')
+            tried_parameters = []
+        end
+        if ~exist('tried_obj_log', 'var')
+            disp('tried_parameters is not defined. Use the default []')
+            tried_parameters = []
+        end
+        pb.X_log = tried_parameters;
+        pb.obj_log = tried_obj_log;
 
-                % Get values of time, original_signals and learned_signalsfrom Python!
-                tried_parameters = pb.X_log;
-                tried_obj_log = pb.obj_log;
+        pb.solve();
+        falses = pb.GetFalse();
 
-                if isempty(falses)
-                    time = [];
-                    original_signals = [];
-                    learned_signals = [];
-                    scores = [];
-                    return;
-                end
+        % Falsification cache
+        tried_parameters = pb.X_log;
+        tried_obj_log = pb.obj_log;
 
-                % Visualize the counter examples
-                % falses.BrSet.PlotSignals();
+        % Times
+        % time = falses.GetTime() % GetTime() seems broken.
+        time = pb.BrSet_Logged.GetTime();
 
-                % Times
-                % time = falses.GetTime() % GetTime() seems broken.
-                time = falses.P.traj{{1}}.time;
+        if isempty(falses)
+            disp('No counter example found!');
+            original_signals = [];
+            learned_signals = [];
+            scores = [];
+            return;
+        end
 
-                """
-            )
-        )
+        % Visualize the counter examples
+        % falses.BrSet.PlotSignals();
 
-        out.write(f"{original_signal_comments}\n")
+        """)
 
-        out.write(
-            textwrap.dedent(
-                f"""\
-                original_signal_names = {original_signal_names};
-                original_signals = falses.GetSignalValues(original_signal_names);
+        w(f"{original_signal_comments}\n")
 
-                """
-            )
-        )
+        wd(f"""\
+        original_signal_names = {original_signal_names};
+        original_signals = falses.GetSignalValues(original_signal_names);
 
-        out.write(f"{learned_signal_comments}\n")
+        all_original_signals = pb.BrSet_Logged.GetSignalValues(original_signal_names);
+        """)
 
-        out.write(
-            textwrap.dedent(
-                f"""\
-                learned_signal_names = {learned_signal_names};
-                learned_signals = falses.GetSignalValues(learned_signal_names);
+        w(f"{learned_signal_comments}\n")
 
-                scores = pb.obj_false;
-                """
-            )
-        )
+        wd(f"""\
+        learned_signal_names = {learned_signal_names};
+        learned_signals = falses.GetSignalValues(learned_signal_names);
+
+        all_learned_signals = pb.BrSet_Logged.GetSignalValues(learned_signal_names);
+
+        scores = pb.obj_false;
+        """)
