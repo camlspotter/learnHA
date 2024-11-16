8abbf8fff94d50d259e449faf060febec8f5d037
Jun Furuse <jun.furuse@dailambda.jp>
2024-11-16 14:48:31 +0900

    tweaks a parameter of test_bball_breach.sh

--- tests/test_bball_breach.sh
+++ tests/test_bball_breach.sh
@@ -21,5 +21,5 @@ pipenv run python loop_breach.py \
      --ode-solver-type fixed --ode-solver FixedStepAuto --invariant-mode 2 \
      \
      -n 30 \
-     --counter-example-threshold 15.0 \
+     --counter-example-threshold 25.0 \
      --max-nloops 5
