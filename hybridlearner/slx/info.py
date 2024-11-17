4e915b96d0814af9d6b874420daf75bfe37538dc
Jun Furuse <jun.furuse@dailambda.jp>
2024-11-17 11:17:31 +0900

    slx.info: stops using tempfiles

--- hybridlearner/slx/info.py
+++ hybridlearner/slx/info.py
@@ -19,47 +19,21 @@ def getvar_list_str(var: str) -> list[str]:
 
 def get_IOports(fn: str) -> tuple[list[str], list[str]]:
     fn = os.path.abspath(fn)
-    with tempfile.NamedTemporaryFile(
-        mode='w', suffix='.m', prefix='get_IOports_'
-    ) as oc:
-        oc.write(
-            textwrap.dedent(
-                f"""\
-                global inports;
-                global outports;
-                inports='dummy';
-                outports='dummy';
-                modelPath = "{fn}";
-                mdl = load_system(modelPath);
-                inports = getfullname(Simulink.findBlocksOfType(mdl, 'Inport'));
-                outports = getfullname(Simulink.findBlocksOfType(mdl, 'Outport'));
-                """
-            )
-        )
-        oc.flush()
-        engine.run(oc.name)
-        return (getvar_list_str('inports'), getvar_list_str('outports'))
+    engine.eval0(f"""
+        modelPath = "{fn}";
+        mdl = load_system(modelPath);
+        opts = Simulink.FindOptions('SearchDepth', 1);
+        inports = getfullname(Simulink.findBlocksOfType(mdl, 'Inport', opts));
+        outports = getfullname(Simulink.findBlocksOfType(mdl, 'Outport', opts));
+    """)
+    return (getvar_list_str('inports'), getvar_list_str('outports'))
 
 
 def get_all_params(fn: str) -> Any:
     fn = os.path.abspath(fn)
-    with tempfile.NamedTemporaryFile(
-        mode='w', suffix='.m', prefix='get_IOports_'
-    ) as oc:
-        oc.write(
-            textwrap.dedent(
-                f"""\
-                % global inports;
-                % global outports;
-                % inports='dummy';
-                % outports='dummy';
-                modelPath = "{fn}";
-                mdl = load_system(modelPath);
-                params = get_param(mdl, 'ObjectParameters');
-                """
-            )
-        )
-        oc.flush()
-        engine.run(oc.name)
-        res = engine.getvar('params')
-        return res
+    engine.eval0(f"""
+        modelPath = '{fn}'; 
+        mdl = load_system(modelPath); 
+        res = get_param(mdl, 'ObjectParameters');
+    """)
+    return engine.getvar('res')
